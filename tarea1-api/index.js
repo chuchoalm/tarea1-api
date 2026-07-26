aa3f1601a5b7ff18c2aa4979344320fbf65b7e9e
chuchoalm <piavig18@gmail.com>
2026-07-26 13:36:10 -0600

    feat(api): agregar endpoint de canción favorita

---
+++ tarea1-api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        nombre: "Pedro Luis Avila Gómez",
+        cancion: "FABULOSA - PIAVI"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor en puerto 3000");
+});
